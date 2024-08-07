981671e3813b4eded7b4cc50872b07a30c224922
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-08-07 13:24:06 +0200

    Creando Edicion

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Products, { loader as productsLoader } from "./views/Products";
 import NewProducts, { action as newProductAction } from "./views/NewProducts";
+import EditProduct from "./views/EditProduct";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
         path: "productos/nuevo",
         element: <NewProducts />,
         action: newProductAction, // This will be called when the "Agregar Producto" link is clicked
+      },
+      {
+        path: "productos/:id/editar", // ROA Pattern - Resource Oriented Design
+        element: <EditProduct />,
       },
     ],
   },
